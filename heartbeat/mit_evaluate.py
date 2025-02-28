f0e7151ab991d9aa0cb088b69f55c5ea1602f326
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-02-28 10:14:51 +0700

    Update ECG report and fix some code
    Remove balanced acc because it is the same as macro recall
    Add heatmap for correlation matrix

--- heartbeat/mit_evaluate.py
+++ heartbeat/mit_evaluate.py
@@ -1,21 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import joblib
 import sklearn.metrics as metrics 
 import pandas as pd 
 
 def evaluate(X, y, model): 
     y_pred = model.predict(X)
-
-    balanced_accuracy = metrics.balanced_accuracy_score(y_pred=y_pred, y_true=y)
+    
     confusion_matrix = np.round(metrics.confusion_matrix(y, y_pred, normalize="true"),2)
+    accuracy = metrics.accuracy_score(y, y_pred)
     macro_precision = metrics.precision_score(y, y_pred, average="macro")
     macro_recall = metrics.recall_score(y, y_pred, average="macro")
+    macro_f1 = metrics.f1_score(y, y_pred, average='macro')
 
-    print("Confusion matrix: \n", confusion_matrix)
-    print("Balanced Acc: ", balanced_accuracy)
+    print("Accuracy: ", accuracy)
     print("Macro Precision: ", macro_precision)
     print("Macro Recall: ", macro_recall)
+    print("Macro F1: ", macro_f1)
 
+    plt.figure(figsize=(6,6))
+    sns.heatmap(
+        confusion_matrix, 
+        annot=True, 
+        cmap='gray_r', 
+        xticklabels=['N', "S", "V", "F", "Q"], 
+        yticklabels=['N', "S", "V", "F", "Q"],
+        cbar=False, 
+        annot_kws={"size": 20})
+    plt.show()
+    plt.close()
 
 def main(): 
     train = pd.read_csv("data/heartbeat/mitbih_train.csv", header = None)
@@ -26,14 +40,14 @@ def main():
     X_test = test.iloc[:, :-1]
     y_test = test.iloc[:, -1]
 
-    model = joblib.load("model/mit_tree.pkl")
+    model = joblib.load("model/mit_forest3.pkl")
     print(model.get_params())
 
+    print("--------------------------------------")
     print("Evaluate on train set: ")
-    print("--------------------------------------")
     evaluate(X_train, y_train, model)
-    print("Evaluate on test set: ")
     print("--------------------------------------")
+    print("Evaluate on test set: ")
     evaluate(X_test, y_test, model)
 
 
